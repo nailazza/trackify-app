8e85008474d169d0f37ca46bb1c5556bd6a238b2
sltn14 <sultanalexanders@gmail.com>
2025-11-30 09:16:39 +0700

    update

--- lib/utils/env.dart
+++ lib/utils/env.dart
@@ -1,4 +1,4 @@
 class Env {
-  static const supabaseUrl = 'https://tvqkstqtyhfklqdutfrb.supabase.co';
-  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR2cWtzdHF0eWhma2xxZHV0ZnJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0NDg2OTIsImV4cCI6MjA4MDAyNDY5Mn0.JzFBOOMJ7UAPeQATjZ-5fulrXDChTntin1q8-wU5Lgw';
+  static const supabaseUrl = 'https://xurqilynlcalhuleiulv.supabase.co';
+  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1cnFpbHlubGNhbGh1bGVpdWx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0NjQ1NzksImV4cCI6MjA4MDA0MDU3OX0.sXKc_00yD5HQoCem31niJmd3H8ZrLnmMKPpw5QT-QCw';
 }
